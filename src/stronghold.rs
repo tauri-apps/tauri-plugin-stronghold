1d7a6822980c02e00241431bca0c150113f8b52b
Ludea <ludealol.olol@gmail.com>
2022-06-21 13:01:51 +0200

    init_or_check_snapshot fn

--- src/stronghold.rs
+++ src/stronghold.rs
@@ -2,21 +2,18 @@ use async_std::{
     sync::Mutex,
     task::{sleep, spawn},
 };
-pub use engine::vault::RecordId;
 use iota_stronghold as stronghold;
-use stronghold::{
-    procedures::{
-        BIP39Generate, Chain, GenerateKey, KeyType, MnemonicLanguage, Slip10Derive, Slip10DeriveInput, Slip10Generate,
-        StrongholdProcedure,
+
+use engine::{
+    vault::RecordHint,
+    snapshot::{
+        file,
+        logic,
     },
-    Client, ClientError, ClientVault, KeyProvider, Location, SnapshotPath, Store, Stronghold,
 };
-use p2p::{Multiaddr, PeerId};
-use engine::vault::RecordHint;
 
 use once_cell::sync::{Lazy, OnceCell};
 use riker::actors::*;
-use serde::{ser::Serializer, Serialize};
 use zeroize::Zeroize;
 
 use std::{
@@ -34,9 +31,28 @@ struct Password(Vec<u8>);
 
 type SnapshotToPasswordMap = HashMap<PathBuf, Arc<Password>>;
 static PASSWORD_STORE: OnceCell<Arc<Mutex<SnapshotToPasswordMap>>> = OnceCell::new();
-static STRONGHOLD_ACCESS_STORE: OnceCell<Arc<Mutex<HashMap<PathBuf, Instant>>>> = OnceCell::new();
-static CURRENT_SNAPSHOT_PATH: OnceCell<Arc<Mutex<Option<PathBuf>>>> = OnceCell::new();
-static PASSWORD_CLEAR_INTERVAL: OnceCell<Arc<Mutex<Duration>>> = OnceCell::new();
 
-const DEFAULT_PASSWORD_CLEAR_INTERVAL: Duration = Duration::from_millis(0);
+async fn get_password_if_needed(
+    snapshot_path: &Path,
+    password: Option<Arc<Password>>,
+) -> Result<Arc<Password>> {
+    match password {
+        Some(password) => Ok(password),
+        None => get_password(snapshot_path).await,
+    }
+}
 
+async fn get_password(snapshot_path: &Path) -> Result<Arc<Password>> {
+    PASSWORD_STORE
+        .get_or_init(default_password_store)
+        .lock()
+        .await
+        .get(snapshot_path)
+        .cloned()
+        .ok_or(Error::PasswordNotSet)
+}
+
+fn init_or_check_snapshot(name: Option<&str>) {
+    file::get_path(name)
+     .map_err(|error| format!("Failed to create snapshot", error))?;
+}
