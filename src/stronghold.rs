3b07cfbc0065b32e499f04e6f2be44eb8fd39bd8
Ludea <ludealol.olol@gmail.com>
2022-06-21 21:35:39 +0200

    Update stronghold.rs

--- src/stronghold.rs
+++ src/stronghold.rs
@@ -40,6 +40,64 @@ pub async fn set_password_clear_interval(interval: Duration) {
     *clear_interval = interval;
 }
 
+fn default_password_store() -> Arc<Mutex<HashMap<PathBuf, Arc<Password>>>> {
+    thread::spawn(|| {
+        spawn(async {
+            loop {
+                let interval = *PASSWORD_CLEAR_INTERVAL
+                    .get_or_init(|| Arc::new(Mutex::new(DEFAULT_PASSWORD_CLEAR_INTERVAL)))
+                    .lock()
+                    .await;
+                sleep(interval).await;
+
+                if interval.as_nanos() == 0 {
+                    continue;
+                }
+
+                let mut passwords = PASSWORD_STORE
+                    .get_or_init(default_password_store)
+                    .lock()
+                    .await;
+                let access_store = STRONGHOLD_ACCESS_STORE
+                    .get_or_init(Default::default)
+                    .lock()
+                    .await;
+                let mut snapshots_paths_to_clear = Vec::new();
+                for (snapshot_path, _) in passwords.iter() {
+                    // if the stronghold was accessed `interval` ago, we clear the password
+                    if let Some(access_instant) = access_store.get(snapshot_path) {
+                        if access_instant.elapsed() > interval {
+                            snapshots_paths_to_clear.push(snapshot_path.clone());
+                        }
+                    }
+                }
+
+                let current_snapshot_path = &*CURRENT_SNAPSHOT_PATH
+                    .get_or_init(Default::default)
+                    .lock()
+                    .await;
+                for snapshot_path in snapshots_paths_to_clear {
+                    passwords.remove(&snapshot_path);
+                    if let Some(curr_snapshot_path) = current_snapshot_path {
+                        if &snapshot_path == curr_snapshot_path {
+                            let mut runtime = actor_runtime().lock().await;
+                            let _ = clear_stronghold_cache(&mut runtime, true);
+                        }
+                    }
+                    emit_status_change(
+                        &snapshot_path,
+                        &Status {
+                            snapshot: SnapshotStatus::Locked,
+                        },
+                    )
+                    .await;
+                }
+            }
+        })
+    });
+    Default::default()
+}
+
 async fn get_password_if_needed(
     snapshot_path: &Path,
     password: Option<Arc<Password>>,
