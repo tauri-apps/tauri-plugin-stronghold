c29e893486ccb09044765a38f3d2a5a3730fa52a
Ludea <ludealol.olol@gmail.com>
2022-06-22 07:43:50 +0200

    Update stronghold.rs

--- src/stronghold.rs
+++ src/stronghold.rs
@@ -23,102 +23,52 @@ use std::{
     time::{Duration, Instant},
 };
 
-#[derive(PartialEq, Eq, Zeroize)]
-#[zeroize(drop)]
-struct Password(Vec<u8>);
+#[derive(Debug, Parser)]
+pub struct VaultLocation {
+    #[clap(long, help = "The storage location inside the vault")]
+    vault_path: String,
 
-type SnapshotToPasswordMap = HashMap<PathBuf, Arc<Password>>;
-static PASSWORD_STORE: OnceCell<Arc<Mutex<SnapshotToPasswordMap>>> = OnceCell::new();
-
-/// Set the password clear interval.
-/// If the stronghold isn't used after `interval`, the password is cleared and must be set again.
-pub async fn set_password_clear_interval(interval: Duration) {
-    let mut clear_interval = PASSWORD_CLEAR_INTERVAL
-        .get_or_init(|| Arc::new(Mutex::new(DEFAULT_PASSWORD_CLEAR_INTERVAL)))
-        .lock()
-        .await;
-    *clear_interval = interval;
+    #[clap(long, help = "The storage location for a record inside a vault")]
+    record_path: String,
 }
 
-fn default_password_store() -> Arc<Mutex<HashMap<PathBuf, Arc<Password>>>> {
-    thread::spawn(|| {
-        spawn(async {
-            loop {
-                let interval = *PASSWORD_CLEAR_INTERVAL
-                    .get_or_init(|| Arc::new(Mutex::new(DEFAULT_PASSWORD_CLEAR_INTERVAL)))
-                    .lock()
-                    .await;
-                sleep(interval).await;
-
-                if interval.as_nanos() == 0 {
-                    continue;
-                }
-
-                let mut passwords = PASSWORD_STORE
-                    .get_or_init(default_password_store)
-                    .lock()
-                    .await;
-                let access_store = STRONGHOLD_ACCESS_STORE
-                    .get_or_init(Default::default)
-                    .lock()
-                    .await;
-                let mut snapshots_paths_to_clear = Vec::new();
-                for (snapshot_path, _) in passwords.iter() {
-                    // if the stronghold was accessed `interval` ago, we clear the password
-                    if let Some(access_instant) = access_store.get(snapshot_path) {
-                        if access_instant.elapsed() > interval {
-                            snapshots_paths_to_clear.push(snapshot_path.clone());
-                        }
-                    }
-                }
-
-                let current_snapshot_path = &*CURRENT_SNAPSHOT_PATH
-                    .get_or_init(Default::default)
-                    .lock()
-                    .await;
-                for snapshot_path in snapshots_paths_to_clear {
-                    passwords.remove(&snapshot_path);
-                    if let Some(curr_snapshot_path) = current_snapshot_path {
-                        if &snapshot_path == curr_snapshot_path {
-                            let mut runtime = actor_runtime().lock().await;
-                            let _ = clear_stronghold_cache(&mut runtime, true);
-                        }
-                    }
-                    emit_status_change(
-                        &snapshot_path,
-                        &Status {
-                            snapshot: SnapshotStatus::Locked,
-                        },
-                    )
-                    .await;
-                }
-            }
-        })
-    });
-    Default::default()
-}
-
-async fn get_password_if_needed(
-    snapshot_path: &Path,
-    password: Option<Arc<Password>>,
-) -> Result<Arc<Password>> {
-    match password {
-        Some(password) => Ok(password),
-        None => get_password(snapshot_path).await,
+impl VaultLocation {
+    fn from(vault: String, record: String) -> Self {
+        Self {
+            record_path: record,
+            vault_path: vault,
+        }
+    }
+    
+    fn to_location(&self) -> Location {
+        Location::Generic {
+            record_path: self.record_path.clone().into_bytes().to_vec(),
+            vault_path: self.vault_path.clone().into_bytes().to_vec(),
+        }
     }
 }
 
-async fn get_password(snapshot_path: &Path) -> Result<Arc<Password>> {
-    PASSWORD_STORE
-        .get_or_init(default_password_store)
-        .lock()
-        .await
-        .get(snapshot_path)
-        .cloned()
-        .ok_or(Error::PasswordNotSet)
-}
+fn create_snapshot(path: String, client_path: String, output: VaultLocation, key: String) {
+    let stronghold = Stronghold::default();
 
-fn init_or_check_snapshot(name: Option<&str>) {
-    file::get_path(name)
-     .map_err(|error| format!("Failed to create snapshot", error))?;
+    let client_path = client_path.as_bytes().to_vec();
+
+    let client = stronghold
+        .create_client(client_path.clone())
+        .expect("Cannot creat client");
+
+    let output_location = output.to_location();
+
+    let generate_key_procedure = GenerateKey {
+        ty: KeyType::Ed25519,
+        output: output_location,
+    };
+
+    client
+        .execute_procedure(generate_key_procedure)
+        .expect("Running procedure failed");
+
+    stronghold
+        .write_client(client_path)
+        .expect("Store client state into snapshot state failed");
 }
