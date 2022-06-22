c36c42fd7fe5d5a8e57ce8476ab3c4a942f5ae51
Ludea <ludealol.olol@gmail.com>
2022-06-22 12:34:58 +0200

    Update stronghold.rs

--- src/stronghold.rs
+++ src/stronghold.rs
@@ -99,10 +99,11 @@ async fn write_from_store(key: String, value: String) -> Result<(), ClientError>
     Ok(())
 }
 
-async fn read_from_store(key: String) -> Result<String, ClientError> {
+async fn read_from_store(key: String) -> String{
     let client = Client::default();
     let store = client.store();
     
-    let record = store.get(key.as_bytes()).unwrap()? ;
-    Ok(String::from_utf8(record.unwrap().to_string());
+    let record = store.get(key.as_bytes()).unwrap() ;
+    
+    String::from_utf8(record.to_string());
 }
