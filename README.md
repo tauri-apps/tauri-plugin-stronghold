e29975587f0751fb8edc434fd7abfbe11661a0d0
JonasKruckenberg <JonasKruckenberg@users.noreply.github.com>
2023-01-07 12:04:00 +0000

    Update sync.yml
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/3861974889

--- README.md
+++ README.md
@@ -1,81 +1,69 @@
-# Tauri Plugin Stronghold
-![Test](https://github.com/tauri-apps/tauri-plugin-stronghold/workflows/Test/badge.svg)
+![plugin-stronghold](banner.png)
 
-This plugin provides a "classical" Tauri Plugin Interface to the [IOTA Stronghold](https://github.com/iotaledger/stronghold.rs) encrypted database, secure runtime, and peer-to-peer service.
+Store secrets and keys using the [IOTA Stronghold](https://github.com/iotaledger/stronghold.rs) encrypted database and secure runtime.
 
-## Architecture
-This repo shape might appear to be strange, but it is really just a hybrid Rust / Typescript project that recommends a specific type of consumption, namely using GIT as the secure distribution mechanism, and referencing specific unforgeable git hashes. Of course, it can also be consumed via Cargo and NPM.
+## Install
 
-### `/src`
-Rust source code that contains the plugin definition and Stronghold features.
-
-### `/webview-src`
-Typescript source for the /dist folder that provides an API to interface with the rust code.
-
-### `/webview-dist`
-Tree-shakeable transpiled JS to be consumed in a WRY webview.
-
-### `/bindings`
-Forthcoming tauri bindings to other programming languages, like DENO.
-
-## Installation
 There are three general methods of installation that we can recommend.
-1. Pull sources directly from Github using git tags / revision hashes (most secure, good for developement, shown below)
-2. Git submodule install this repo in your tauri project and then use `file` protocol to ingest the source
-3. Use crates.io and npm (easiest, and requires you to trust that our publishing pipeline worked)
 
-For more details and usage see [the svelte demo](examples/svelte-app/src/App.svelte). Please note, below in the dependencies you can also lock to a revision/tag in both the `Cargo.toml` and `package.json`
+1. Use crates.io and npm (easiest, and requires you to trust that our publishing pipeline worked)
+2. Pull sources directly from Github using git tags / revision hashes (most secure)
+3. Git submodule install this repo in your tauri project and then use file protocol to ingest the source (most secure, but inconvenient to use)
 
-### RUST
+Install the Core plugin by adding the following to your `Cargo.toml` file:
+
 `src-tauri/Cargo.toml`
-```yaml
-[dependencies.tauri-plugin-stronghold]
-git = "https://github.com/tauri-apps/tauri-plugin-stronghold"
-tag = "v0.1.0"
-#branch = "main"
+
+```toml
+[dependencies]
+tauri-plugin-stronghold = { git = "https://github.com/tauri-apps/plugins-workspace", branch = "dev" }
 ```
 
-Use in `src-tauri/src/main.rs`:
+You can install the JavaScript Guest bindings using your preferred JavaScript package manager:
+
+> Note: Since most JavaScript package managers are unable to install packages from git monorepos we provide read-only mirrors of each plugin. This makes installation option 2 more ergonomic to use.
+
+```sh
+pnpm add https://github.com/tauri-apps/tauri-plugin-stronghold
+# or
+npm add https://github.com/tauri-apps/tauri-plugin-stronghold
+# or
+yarn add https://github.com/tauri-apps/tauri-plugin-stronghold
+```
+
+## Usage
+
+First you need to register the core plugin with Tauri:
+
+`src-tauri/src/main.rs`
+
 ```rust
 fn main() {
     tauri::Builder::default()
-        .plugin(
-          tauri_plugin_stronghold::Builder::new(|password| {
+        .plugin(tauri_plugin_stronghold::Builder::new(|password| {
             // TODO: hash the password here with e.g. argon2, blake2b or any other secure algorithm
-            unimplemented!()
-          })
-          .build()
-        )
-        .build()
-        .run();
+            todo!()
+        })
+        .build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 ```
 
-### WEBVIEW
-`Install from a tagged release`
-```
-npm install github:tauri-apps/tauri-plugin-stronghold#v0.2.0
-# or
-yarn add github:tauri-apps/tauri-plugin-stronghold#v0.2.0
+Afterwards all the plugin's APIs are available through the JavaScript guest bindings:
+
+```javascript
+import { Stronghold, Location } from "tauri-plugin-stronghold-api";
+
+// TODO
 ```
 
-`Install from a commit`
-```
-npm install github:tauri-apps/tauri-plugin-stronghold#6749525a47a95439c9703d3a49b94ac65660998f
-# or
-yarn add github:tauri-apps/tauri-plugin-stronghold#6749525a47a95439c9703d3a49b94ac65660998f
-```
+## Contributing
 
-`package.json`
-```json
-  "dependencies": {
-    "tauri-plugin-stronghold-api": "github:tauri-apps/tauri-plugin-stronghold#v0.1.0",
-```
+PRs accepted. Please make sure to read the Contributing Guide before making a pull request.
 
-Use within your JS/TS:
-```ts
-import { Stronghold, Location } from 'tauri-plugin-stronghold-api'
-```
+## License
 
-# License
-MIT / Apache-2.0
+Code: (c) 2015 - Present - The Tauri Programme within The Commons Conservancy.
+
+MIT or MIT/Apache 2.0 where applicable.
---
+++ guest-js/dist/index.d.ts
@@ -0,0 +1,191 @@
+type BytesDto = string | number[];
+export type ClientPath = string | Iterable<number> | ArrayLike<number> | ArrayBuffer;
+export type VaultPath = string | Iterable<number> | ArrayLike<number> | ArrayBuffer;
+export type RecordPath = string | Iterable<number> | ArrayLike<number> | ArrayBuffer;
+export type StoreKey = string | Iterable<number> | ArrayLike<number> | ArrayBuffer;
+export interface ConnectionLimits {
+    maxPendingIncoming?: number;
+    maxPendingOutgoing?: number;
+    maxEstablishedIncoming?: number;
+    maxEstablishedOutgoing?: number;
+    maxEstablishedPerPeer?: number;
+    maxEstablishedTotal?: number;
+}
+export interface PeerAddress {
+    known: string[];
+    use_relay_fallback: boolean;
+}
+export interface AddressInfo {
+    peers: Map<string, PeerAddress>;
+    relays: string[];
+}
+export interface ClientAccess {
+    useVaultDefault?: boolean;
+    useVaultExceptions?: Map<VaultPath, boolean>;
+    writeVaultDefault?: boolean;
+    writeVaultExceptions?: Map<VaultPath, boolean>;
+    cloneVaultDefault?: boolean;
+    cloneVaultExceptions?: Map<VaultPath, boolean>;
+    readStore?: boolean;
+    writeStore?: boolean;
+}
+export interface Permissions {
+    default?: ClientAccess;
+    exceptions?: Map<VaultPath, ClientAccess>;
+}
+export interface NetworkConfig {
+    requestTimeout?: Duration;
+    connectionTimeout?: Duration;
+    connectionsLimit?: ConnectionLimits;
+    enableMdns?: boolean;
+    enableRelay?: boolean;
+    addresses?: AddressInfo;
+    peerPermissions?: Map<string, Permissions>;
+    permissionsDefault?: Permissions;
+}
+/** A duration definition. */
+export interface Duration {
+    /** The number of whole seconds contained by this Duration. */
+    secs: number;
+    /** The fractional part of this Duration, in nanoseconds. Must be greater or equal to 0 and smaller than 1e+9 (the max number of nanoseoncds in a second)*/
+    nanos: number;
+}
+export declare class Location {
+    type: string;
+    payload: Record<string, unknown>;
+    constructor(type: string, payload: Record<string, unknown>);
+    static generic(vault: VaultPath, record: RecordPath): Location;
+    static counter(vault: VaultPath, counter: number): Location;
+}
+declare class ProcedureExecutor {
+    procedureArgs: Record<string, unknown>;
+    constructor(procedureArgs: Record<string, unknown>);
+    /**
+     * Generate a SLIP10 seed for the given location.
+     * @param outputLocation Location of the record where the seed will be stored.
+     * @param sizeBytes The size in bytes of the SLIP10 seed.
+     * @param hint The record hint.
+     * @returns
+     */
+    generateSLIP10Seed(outputLocation: Location, sizeBytes?: number): Promise<Uint8Array>;
+    /**
+     * Derive a SLIP10 private key using a seed or key.
+     * @param chain The chain path.
+     * @param source The source type, either 'Seed' or 'Key'.
+     * @param sourceLocation The source location, must be the `outputLocation` of a previous call to `generateSLIP10Seed` or `deriveSLIP10`.
+     * @param outputLocation Location of the record where the private key will be stored.
+     * @param hint The record hint.
+     * @returns
+     */
+    deriveSLIP10(chain: number[], source: "Seed" | "Key", sourceLocation: Location, outputLocation: Location): Promise<Uint8Array>;
+    /**
+     * Store a BIP39 mnemonic.
+     * @param mnemonic The mnemonic string.
+     * @param outputLocation The location of the record where the BIP39 mnemonic will be stored.
+     * @param passphrase The optional mnemonic passphrase.
+     * @param hint The record hint.
+     * @returns
+     */
+    recoverBIP39(mnemonic: string, outputLocation: Location, passphrase?: string): Promise<Uint8Array>;
+    /**
+     * Generate a BIP39 seed.
+     * @param outputLocation The location of the record where the BIP39 seed will be stored.
+     * @param passphrase The optional mnemonic passphrase.
+     * @param hint The record hint.
+     * @returns
+     */
+    generateBIP39(outputLocation: Location, passphrase?: string): Promise<Uint8Array>;
+    /**
+     * Gets the Ed25519 public key of a SLIP10 private key.
+     * @param privateKeyLocation The location of the private key. Must be the `outputLocation` of a previous call to `deriveSLIP10`.
+     * @returns A promise resolving to the public key hex string.
+     */
+    getEd25519PublicKey(privateKeyLocation: Location): Promise<Uint8Array>;
+    /**
+     * Creates a Ed25519 signature from a private key.
+     * @param privateKeyLocation The location of the record where the private key is stored. Must be the `outputLocation` of a previous call to `deriveSLIP10`.
+     * @param msg The message to sign.
+     * @returns A promise resolving to the signature hex string.
+     */
+    signEd25519(privateKeyLocation: Location, msg: string): Promise<Uint8Array>;
+}
+export declare class Client {
+    path: string;
+    name: BytesDto;
+    constructor(path: string, name: ClientPath);
+    /**
+     * Get a vault by name.
+     * @param name
+     * @param flags
+     * @returns
+     */
+    getVault(name: VaultPath): Vault;
+    getStore(): Store;
+}
+export declare class Store {
+    path: string;
+    client: BytesDto;
+    constructor(path: string, client: BytesDto);
+    get(key: StoreKey): Promise<Uint8Array>;
+    insert(key: StoreKey, value: number[], lifetime?: Duration): Promise<void>;
+    remove(key: StoreKey): Promise<Uint8Array | null>;
+}
+/**
+ * A key-value storage that allows create, update and delete operations.
+ * It does not allow reading the data, so one of the procedures must be used to manipulate
+ * the stored data, allowing secure storage of secrets.
+ */
+export declare class Vault extends ProcedureExecutor {
+    /** The vault path. */
+    path: string;
+    client: BytesDto;
+    /** The vault name. */
+    name: BytesDto;
+    constructor(path: string, client: ClientPath, name: VaultPath);
+    /**
+     * Insert a record to this vault.
+     * @param location The record location.
+     * @param record  The record data.
+     * @param recordHint The record hint.
+     * @returns
+     */
+    insert(recordPath: RecordPath, secret: number[]): Promise<void>;
+    /**
+     * Remove a record from the vault.
+     * @param location The record location.
+     * @param gc Whether to additionally perform the gargage collection or not.
+     * @returns
+     */
+    remove(location: Location): Promise<void>;
+}
+/**
+ * A representation of an access to a stronghold.
+ */
+export declare class Stronghold {
+    path: string;
+    /**
+     * Initializes a stronghold.
+     * If the snapshot path located at `path` exists, the password must match.
+     * @param path
+     * @param password
+     */
+    constructor(path: string, password: string);
+    /**
+     * Force a reload of the snapshot. The password must match.
+     * @param password
+     * @returns
+     */
+    private reload;
+    /**
+     * Remove this instance from the cache.
+     */
+    unload(): Promise<void>;
+    loadClient(client: ClientPath): Promise<Client>;
+    createClient(client: ClientPath): Promise<Client>;
+    /**
+     * Persists the stronghold state to the snapshot.
+     * @returns
+     */
+    save(): Promise<void>;
+}
+export {};
