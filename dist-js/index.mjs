5e4161b10d847af6a23cae7b7bccec7ab207557a
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-06-06 11:50:58 +0000

    fix(stronghold): remove constructor, add static load function (#416)
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/5188163917
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -252,20 +252,19 @@ class Stronghold {
      * @param path
      * @param password
      */
-    constructor(path, password) {
+    constructor(path) {
         this.path = path;
-        void this.reload(password);
     }
     /**
-     * Force a reload of the snapshot. The password must match.
+     * Load the snapshot if it exists (password must match), or start a fresh stronghold instance otherwise.
      * @param password
      * @returns
      */
-    async reload(password) {
+    static async load(path, password) {
         return await invoke("plugin:stronghold|initialize", {
-            snapshotPath: this.path,
+            snapshotPath: path,
             password,
-        });
+        }).then(() => new Stronghold(path));
     }
     /**
      * Remove this instance from the cache.
